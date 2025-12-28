04e4ab953a7a723a06085132f93fb8e28d68f18b
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-28 20:10:27 +0530

    ShadCN Dark Mode Tutorial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Link from "next/link";
-import { LogOut, Moon, Settings, SquareMenu, User } from "lucide-react";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 import {
@@ -11,8 +13,10 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
+  const { setTheme } = useTheme();
   return (
     <nav className="flex p-4 items-center justify-between">
       {/* Left Side */}
@@ -20,8 +24,29 @@ const Navbar = () => {
       {/* Right Side */}
       <div className="flex items-center gap-4 ">
         <Link href="/">Dashboard</Link>
-        <Moon />
+        {/* THEME MENU */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
 
+        {/* USER MENU */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Avatar>
@@ -41,22 +66,6 @@ const Navbar = () => {
             <DropdownMenuItem variant="destructive">
               <LogOut /> Logout
             </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
-              <SquareMenu />
-              {/* sr-only sirf blind logo ke liye  */}
-              <span className="sr-only">open menu</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem>menu item 1</DropdownMenuItem>
-            <DropdownMenuItem>menu item 2</DropdownMenuItem>
-            <DropdownMenuItem>menu item 3</DropdownMenuItem>
-            <DropdownMenuItem>menu item 4</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
