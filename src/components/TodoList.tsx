5028e6219709d4a8fc0f610f9d108a4c7992e76b
ashutosh mishra <mishraashutosh.dev@gmail.com>
2025-12-30 16:46:29 +0530

    ShadCN Calendar and Popover Tutorial and date-fns

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,41 @@
+"use client";
+
+import { useState } from "react";
 import { Card } from "./ui/card";
 import { Checkbox } from "./ui/checkbox";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { ScrollArea } from "./ui/scroll-area";
+import { Button } from "./ui/button";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { Calendar } from "./ui/calendar";
 
 const TodoList = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [open, setOpen] = useState(false);
   return (
-    <div>
-      calendar
+    <div className="">
+      <h1 className="text-lg font-medium mb-6">Todo List</h1>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button className="w-full">
+            <CalendarIcon />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 w-auto">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={(date) => {
+              setDate(date);
+              setOpen(false);
+            }}
+            // className="rounded-md border shadow-sm"
+            // captionLayout="dropdown"
+          />
+        </PopoverContent>
+      </Popover>
       {/* LIST */}
       <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
         <div className="flex flex-col gap-4">
